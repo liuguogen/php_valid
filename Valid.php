64f3652c9f643d7df61a3282a0f66c29cd97f598
liuguogen <liuguogen@yundun.com>
2019-11-22 14:29:30 +0800

    【修复】

--- Valid.php
+++ Valid.php
@@ -1,5 +1,5 @@
 <?php
-namespace Valid;
+namespace Validation;
 // +----------------------------------------------------------------------
 // |数据验证类
 // +----------------------------------------------------------------------
@@ -28,7 +28,62 @@ class Valid
     protected $field = [];
 
     // 验证规则默认提示信息
-    protected static $typeMsg = [];
+    protected static $typeMsg = [
+            'must'        => ['msg'=>':attribute不能为空','code'=>10001],
+            'num'         => ['msg'=>':attribute必须是数字','code'=>10002],
+            'int'         => ['msg'=>':attribute必须是整数','code'=>10003],
+            'short'       => ['msg'=>':attribute必须是短整数','code'=>10004],
+            'float'       => ['msg'=>':attribute必须是浮点数','code'=>10005],
+            'str'         => ['msg'=>':attribute必须是字符串','code'=>10006],
+            'gte'         => ['msg'=>':attribute必须大于等于 :rule','code'=>10007],
+            'gt'          => ['msg'=>':attribute必须大于 :rule','code'=>10008],
+            'lte'         => ['msg'=>':attribute必须小于等于 :rule','code'=>10009],
+            'lt'          => ['msg'=>':attribute必须小于 :rule','code'=>10010],
+            'eq'          => ['msg'=>':attribute必须等于 :rule','code'=>10011],
+            'neq'         => ['msg'=>':attribute必须不等于 :rule','code'=>10012],
+            'gt0'         => ['msg'=>':attribute必须大于0','code'=>100013],
+            'bool'        => ['msg'=>':attribute必须是布尔值','code'=>10014],
+            'regex'       => ['msg'=>':attribute不符合指定规则','code'=>10015],
+            'list'        => ['msg'=>':attribute必须是数组','code'=>10016],
+            'mustKey'     => ['msg'=>':attribute必须存在 :rule','code'=>10017],
+            'date'        => ['msg'=>':attribute格式不符合','code'=>10018],
+            'dateFormat'  => ['msg'=>':attribute必须使用日期格式 :rule','code'=>10019],
+            'after'       => ['msg'=>':attribute日期不能小于 :rule','code'=>10020],
+            'before'      => ['msg'=>':attribute日期不能超过 :rule','code'=>10021],
+            'expire'      => ['msg'=>'不在有效期内 :rule','code'=>10022],
+            'file'        => ['msg'=>':attribute不是有效的上传文件','code'=>10023],
+            'fileSize'    => ['msg'=>'上传文件大小不符','code'=>10024],
+            'fileExt'     => ['msg'=>'上传文件后缀不符','code'=>10025],
+            'fileMime'    => ['msg'=>'上传文件类型不符','code'=>10026],
+            'image'       => ['msg'=>':attribute不是有效的图像文件','code'=>10027],
+            'strIn'       => ['msg'=>':attribute格式不符','code'=>10028],
+            'strIs'       => ['msg'=>':attribute格式不符','code'=>10029],
+            'len_gt'      => ['msg'=>':attribute长度必须大于 :rule','code'=>10030],
+            'len_gte'     => ['msg'=>':attribute长度必须大于等于 :rule','code'=>10031],
+            'len_lt'      => ['msg'=>':attribute长度必须小于 :rule','code'=>10032],
+            'len_lte'     => ['msg'=>':attribute长度必须小于等于 :rule','code'=>10033],
+            'suffix'      => ['msg'=>':attribute没有后缀 :rule','code'=>10034],
+            'prefix'      => ['msg'=>':attribute没有并锥 :rule','code'=>10035],
+            'different'   => ['msg'=>':attribute和比较字段:2不能相同','code'=>10036],
+            'in'          => ['msg'=>':attribute必须在 :rule 范围内','code'=>10037],
+            'not_in'      => ['msg'=>':attribute不能在 :rule 范围内','code'=>10038],
+            'count_gt'    => ['msg'=>':attribute数组必须大于 :rule','code'=>10039],
+            'count_gte'   => ['msg'=>':attribute数组必须大于等于 :rule','code'=>10040],
+            'count_lt'    => ['msg'=>':attribute数组必须小于 :rule','code'=>10041],
+            'count_lte'   => ['msg'=>':attribute数组必须小于等于 :rule','code'=>10042],
+            'unique'      => ['msg'=>':attribute有重复值存在 :rule','code'=>10043],
+            'range'       => ['msg'=>':attribute只能在 :1 - :2 之间','code'=>10044],
+            'not_range'   => ['msg'=>':attribute不能在 :1 - :2 之间','code'=>10045],
+            'length'      => ['msg'=>':attribute长度不符合要求 :rule','code'=>10046],
+            'max'         => ['msg'=>':attribute长度不能超过 :rule','code'=>10047],
+            'min'         => ['msg'=>':attribute长度不能小于 :rule','code'=>10048],
+            'allowIp'     => ['msg'=>'不允许的IP访问','code'=>10049],
+            'denyIp'      => ['msg'=>'禁止的IP访问','code'=>10050],
+            'confirm'     => ['msg'=>':attribute和确认字段:2不一致','code'=>10051],
+            'mustIf'   => ['msg'=>':attribute不能为空','code'=>10052],// 验证某个字段的值等于某个值的时候必须 requireIf:field,value  'password'=>'requireIf:account,1' 当account的值等于1的时候 password必须
+            'mustWith' => ['msg'=>':attribute不能为空','code'=>10053],// 验证某个字段有值的时候必须 requireWith:field 'password'=>'requireWith:account' // 当account有值的时候password字段必须
+            'mustCallback' => ['msg'=>':attribute不能为空','code'=>10054],// 验证当某个callable为真的时候字段必须  requireCallback:callable 使用check_require方法检查是否需要验证age字段必须'age'=>'requireCallback:check_require|number'
+    ];
 
     // 正则表达式 regex = ['zip'=>'\d{6}',...]
     protected $regex = [];
@@ -51,13 +106,9 @@ class Valid
 
     public function __construct(array $rules = [], $message = [], $field = [])
     {
-
-        $validation           = require  . 'config/valid.php';
         $this->rule    = array_merge($this->rule, $rules);
-
         $this->message = array_merge($this->message, $message);
         $this->field   = array_merge($this->field, $field);
-        self::$typeMsg = $validation;
     }
 
     /**
@@ -68,31 +119,48 @@ class Valid
      * @param array     $field 验证字段描述信息
      * @return Validate
      */
-    public static  function make($rules = [], $data=[],&$message = [], $field = [])
+    public   function make($rules = [], $data=[],&$message = [], $field = [])
     {
 
-        $validate = new self($rules, $message, $field);
+        $validate = new self($rules, $message ? : [], $field ? :[] );
         
         if(!$validate->check($data,$validate->rule)) {
             $message = $validate->getError();
         }else {
             $message = [];
-            foreach ($data as $key => $value) {
-                if(!in_array($key, array_keys($rules))) {
-                    unset($data[$key]);
-                }
-            }
-            foreach ($rules as $key => $value) {
-                if(isset($value['default']) && ($value['default']!='' || !empty($value['default']) || in_array($value['default'], [0,'0']) ) && !$data[$key] ) {
-                    $data[$key] = $value['default'];
-                }
-            }
+            
+            $validate->__returnData($data,$rules);
             return $data;
         }
         
         
     }
     
+    /**
+     * @param  获取数据
+     * @param  [type]
+     * @return [type]
+     */
+    private function __returnData(&$data,$rules) {
+
+        if(is_array($data)) {
+            foreach ($data as $key => $value) {
+                if(!in_array($key, array_keys($rules))) {
+                    unset($data[$key]);
+                }
+            }
+            
+            foreach ($rules as $key => $value) {
+                if(isset($value['child']) && is_array($value['child']) && $value['child']) {
+                    
+                    $this->__returnData($data[$key],$value['child']);
+                }
+                if(isset($value['default']) && ($value['default']!='' || !empty($value['default']) || in_array($value['default'], [0,'0']) ) && !$data[$key] ) {
+                        $data[$key] = $value['default'];
+                }
+            }
+        }
+    }
     /**
      * 添加字段验证规则
      * @access protected
@@ -172,12 +240,14 @@ class Valid
     {
 
 
-
-        foreach($data as $k => &$v) {
-           if(json_decode($v,1)) {
-                $v = json_decode($v,1);
-           }
+        if(is_array($data)) {
+            foreach($data as $k => &$v) {
+               if(is_string($v) && json_decode($v,1) ) {
+                    $v = json_decode($v,1);
+               }
+            }
         }
+        
         
         $this->error = [];
 
@@ -189,13 +259,19 @@ class Valid
        
         foreach ($rules as $key => $item) {
             
-            $rule = $item['vstr'];
+            $rule = $item['valid'];
             $type = $item['type'];
             if(!$rule || !$type) continue;
             if (isset($item['msg'])) {
                 $msg = is_string($item['msg']) ? explode('|', $item['msg']) : $item['msg'];
             } else {
                 $msg = [];
+            }
+            if (strpos($key, '|')) {
+                // 字段|描述 用于指定属性名称
+                list($key, $title) = explode('|', $key);
+            } else {
+                $title = isset($this->field[$key]) ? $this->field[$key] : $key;
             }
             $value = $this->getDataValue($data, $key);
             if('mix' != $type ) {
@@ -207,12 +283,7 @@ class Valid
                 }
             }
 
-            if (strpos($key, '|')) {
-                // 字段|描述 用于指定属性名称
-                list($key, $title) = explode('|', $key);
-            } else {
-                $title = isset($this->field[$key]) ? $this->field[$key] : $key;
-            }
+            
 
             
             if(!strpos($rule, ':') && $rule == 'in') {
@@ -223,7 +294,7 @@ class Valid
                     $rule = 'in'.':'.implode(',', array_keys($item['in_kdata']));
                 }
             }
-            // 获取数据 支持二维数组
+            // 获取数据 
             $value = $this->getDataValue($data, $key);
 
             if(strpos($rule, ':')) {
@@ -342,7 +413,7 @@ class Valid
     {
 
 
-        // 支持多规则验证 require|in:a,b,c|... 或者 ['require','in'=>'a,b,c',...]
+        // 支持多规则验证 must|in:0,1,2
         if (is_string($rules)) {
             $rules = explode('|', $rules);
         }
@@ -630,7 +701,7 @@ class Valid
         switch ($rule) {
             case 'must':
                 // 必须
-                $result = !empty($value) || '0' == $value;
+                $result = !empty($value) || '0' == $value || $value!='';
                 break;
             
             case 'date':
@@ -663,8 +734,9 @@ class Valid
                 $result = in_array($value, [true, false, 0, 1, '0', '1'], true);
                 break;
             case 'list':
+            
                 // 是否为数组
-                $result = is_array($value);
+                $result =  !empty($value) && is_array($value) &&  $value !='';
                 break;
             case 'file':
                 $result = $value instanceof File;
